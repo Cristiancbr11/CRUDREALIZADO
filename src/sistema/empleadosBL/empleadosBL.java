919122286e2aea573850b076e44ef71a91a41a4e
Cristiancbr11 <cristiancbr2119@gmail.com>
2022-04-22 18:03:27 -0500

    Campo apellido

---
+++ src/sistema/empleadosBL/empleadosBL.java
@@ -0,0 +1,47 @@
+
+package sistema.empleadosBL;
+
+/**
+ *Creamos los métodos setters y getters de cada elemento
+ * @author Daniel
+ */
+public class empleadosBL {
+    
+    int ID;
+    String nombre, correo, apellido;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
+    
+}
